e1a883fefaa499b50017691f24bf9cd547f3c0ae
nazeeruddeen <nazeershaik.ai@gmail.com>
2026-03-06 11:30:32 +0530

    first commit

---
+++ src/main/java/com/pipeline/jenkins/JenkinsController.java
@@ -0,0 +1,19 @@
+package com.pipeline.jenkins;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("jenkins")
+public class JenkinsController {
+
+    @GetMapping("welcome")
+    public String welcome(){
+        return "Welcome to Jenkins!";
+    }
+    @GetMapping("hello")
+    public String hello(){
+        return "Hello World!";
+    }
+}
